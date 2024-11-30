7adaaa0b3cce344787261d50a6dce69ec284a250
Viktor Grunwaldt <v.gruenwaldt@protonmail.com>
2024-11-30 03:45:51 +0100

    btrfs subvolumes

--- disko.nix
+++ disko.nix
@@ -2,14 +2,16 @@
   disko.devices = {
     disk = {
       main = {
-        device = "/dev/nvme0n1";
+        device = "/dev/nvme0n1/1";
         type = "disk";
         content = {
           type = "gpt";
           partitions = {
             ESP = {
+              priority = 1;
               type = "EF00";
-              size = "512M";
+              start = "1M";
+              end = "512M";
               content = {
                 type = "filesystem";
                 format = "vfat";
@@ -20,9 +22,30 @@
             root = {
               size = "100%";
               content = {
-                type = "filesystem";
-                format = "btrfs";
-                mountpoint = "/";
+                type = "btrfs";
+                extraArgs = [ "-f" ]; # Override existing partition
+                # Subvolumes must set a mountpoint in order to be mounted,
+                # unless their parent is mounted
+                subvolumes = {
+                  # Subvolume name is different from mountpoint
+                  "/rootfs" = {
+                    mountpoint = "/";
+                  };
+                  # Subvolume name is the same as the mountpoint
+                  "/home" = {
+                    # mountOptions = [ "compress=zstd" ];
+                    mountpoint = "/home";
+                  };
+                  # Sub(sub)volume doesn't need a mountpoint as its parent is mounted
+                  "/home/user" = { };
+                  # Parent is not mounted so the mountpoint must be set
+                  "/nix" = {
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountpoint = "/nix";
+                  };
+                };
+
+                mountpoint = "/partition-root";
               };
             };
           };

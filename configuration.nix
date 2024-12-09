4cf7ba9feef60dc1dbe477d1bf415c773e280c9b
Viktor Grunwaldt <v.gruenwaldt@protonmail.com>
2024-12-09 19:05:51 +0100

    init sway using systemd-cat

--- configuration.nix
+++ configuration.nix
@@ -234,7 +234,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --cmd 'systemd-cat -t sway sway'";
         user = "vi";
       };
     };

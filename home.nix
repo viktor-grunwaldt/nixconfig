07bf7026f106cf9f4ae90430908e897a5b8ef483
Viktor Grunwaldt <v.gruenwaldt@protonmail.com>
2025-04-02 13:51:50 +0200

    chore: install uni stuff

--- home.nix
+++ home.nix
@@ -77,7 +77,7 @@ in
     xarchiver
     jq
     (writeShellScriptBin "projector-toggle" (builtins.readFile ./projector-toggle.sh))
-
+    renode
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of

af8b6f2f8fccfa41dddf67a1e8aa610237dd5954
Viktor Grunwaldt <v.gruenwaldt@protonmail.com>
2025-05-26 16:38:02 +0200

    add: noatime

--- disko.nix
+++ disko.nix
@@ -33,7 +33,7 @@
                   };
                   # Subvolume name is the same as the mountpoint
                   "/home" = {
-                    mountOptions = [ "compress=zstd" ];
+                    mountOptions = [ "compress=zstd" "noatime" ];
                     mountpoint = "/home";
                   };
                   # Parent is not mounted so the mountpoint must be set

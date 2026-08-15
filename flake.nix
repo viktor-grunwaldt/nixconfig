93cc206431ff08d08e75f6c94c62ec7f25f7ff4a
Viktor Grunwaldt <v.gruenwaldt@protonmail.com>
2026-08-15 22:16:48 +0200

    Use nixos.channel link instead of github API

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
     catppuccin.url = "github:catppuccin/nix";
     home-manager = {
       url = "github:nix-community/home-manager";
